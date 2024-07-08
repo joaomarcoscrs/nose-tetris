c199f28ed165c1da093a23c78d433fedda13f581
João <joaomarcosxix@gmail.com>
2024-07-07 22:54:47 -0300

    basic loading state

--- game/main.py
+++ game/main.py
@@ -3,7 +3,7 @@ import threading
 
 from .tetris import Tetris
 from .settings import initialize_settings, SCREEN_WIDTH, SCREEN_HEIGHT, FALL_SPEED
-from .colors import BLACK
+from .colors import BLACK, WHITE
 from .controls import KeysControl, NoseControl
 from .roboflow import main as inference_main
 
@@ -23,18 +23,21 @@ pygame.display.set_caption('Tetris')
 # Clock
 clock = pygame.time.Clock()
 
-EVENT_TIMEOUT = 150
+EVENT_TIMEOUT = 100
 LAST_EVENT_TIME = 0
 LATEST_IMAGE = None
+predictions_received = False  # Flag to track if predictions have been received
 
 def post_predictions_event(predictions: dict, image: Union[None, VideoFrame]):
     global LAST_EVENT_TIME
     global EVENT_TIMEOUT
     global LATEST_IMAGE
+    global predictions_received
     
     LATEST_IMAGE = image
-    # Only posts 1 event every EVENT_TIMEOUT ms
+    predictions_received = True  # Set the flag to True once predictions are received
     
+    # Only posts 1 event every EVENT_TIMEOUT ms
     if pygame.time.get_ticks() - LAST_EVENT_TIME < EVENT_TIMEOUT:
         return
       
@@ -54,56 +57,63 @@ def game_main():
     running = True
     while running:
         screen.fill(BLACK)
-        tetris.draw_grid(screen)
-        tetris.draw_shape(screen)
         
-        
-        # Draw the latest image at the top right corner
-        if LATEST_IMAGE is not None:
-            # Calculate new dimensions
-            new_width = SCREEN_WIDTH // 4
-            aspect_ratio = LATEST_IMAGE.shape[0] / LATEST_IMAGE.shape[1]
-            new_height = int(new_width * aspect_ratio)
+        if predictions_received:
+            tetris.draw_grid(screen)
+            tetris.draw_shape(screen)
             
-            # Resize the image
-            image_surface = pygame.surfarray.make_surface(LATEST_IMAGE.swapaxes(0, 1))
-            image_surface = pygame.transform.scale(image_surface, (new_width, new_height))
-            # Mirror the image horizontally
-            image_surface = pygame.transform.flip(image_surface, True, False)
-            
-            # Blit the image to the screen
-            screen.blit(image_surface, (SCREEN_WIDTH - new_width, 0))
+            # Draw the latest image at the top right corner
+            if LATEST_IMAGE is not None:
+                # Calculate new dimensions
+                new_width = SCREEN_WIDTH // 4
+                aspect_ratio = LATEST_IMAGE.shape[0] / LATEST_IMAGE.shape[1]
+                new_height = int(new_width * aspect_ratio)
+                
+                # Resize the image
+                image_surface = pygame.surfarray.make_surface(LATEST_IMAGE.swapaxes(0, 1))
+                image_surface = pygame.transform.scale(image_surface, (new_width, new_height))
+                # Mirror the image horizontally
+                image_surface = pygame.transform.flip(image_surface, True, False)
+                
+                # Blit the image to the screen
+                screen.blit(image_surface, (SCREEN_WIDTH - new_width, 0))
+        else:
+            # Render loading state
+            font = pygame.font.SysFont(None, 55)
+            text_surface = font.render('Loading...', True, WHITE)
+            screen.blit(text_surface, (SCREEN_WIDTH // 2 - text_surface.get_width() // 2, SCREEN_HEIGHT // 2 - text_surface.get_height() // 2))
         
         pygame.display.flip()
 
-        for event in pygame.event.get():
-            # Change the controller being used here. It should have the same interface as BaseControl and KeysControl 
-            controls = [KeysControl(event), NoseControl(event)]
-            
-            for control in controls:
-                if event.type == pygame.QUIT:
-                    running = False
-                elif control.activated():
-                    # left/right moves are exclusive
-                    if control.move_left() and tetris.is_valid_move(-1, 0):
-                        tetris.move_shape(-1, 0)
-                    elif control.move_right() and tetris.is_valid_move(1, 0):
-                        tetris.move_shape(1, 0)
-                    # up/down moves are exclusive
-                    if control.move_down() and tetris.is_valid_move(0, 1):
-                        tetris.move_shape(0, 1)
-                    elif control.rotate():
-                        tetris.rotate_shape()
-                    if control.quit_game():
-                        running = False  # Exit full screen mode on pressing ESC
+        if predictions_received:
+            for event in pygame.event.get():
+                # Change the controller being used here. It should have the same interface as BaseControl and KeysControl 
+                controls = [KeysControl(event), NoseControl(event)]
+                
+                for control in controls:
+                    if event.type == pygame.QUIT:
+                        running = False
+                    elif control.activated():
+                        # left/right moves are exclusive
+                        if control.move_left() and tetris.is_valid_move(-1, 0):
+                            tetris.move_shape(-1, 0)
+                        elif control.move_right() and tetris.is_valid_move(1, 0):
+                            tetris.move_shape(1, 0)
+                        # up/down moves are exclusive
+                        if control.move_down() and tetris.is_valid_move(0, 1):
+                            tetris.move_shape(0, 1)
+                        elif control.rotate():
+                            tetris.rotate_shape()
+                        if control.quit_game():
+                            running = False  # Exit full screen mode on pressing ESC
 
-        fall_time += clock.get_time()
-        if fall_time > FALL_SPEED:
-            tetris.update()
-            fall_time = 0
+            fall_time += clock.get_time()
+            if fall_time > FALL_SPEED:
+                tetris.update()
+                fall_time = 0
 
-        if tetris.game_over:
-            running = False
+            if tetris.game_over:
+                running = False
 
         clock.tick(30)
 
