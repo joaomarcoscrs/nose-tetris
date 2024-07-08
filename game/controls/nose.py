e6a59c12489d63434f491be82b9dd1e5b6fb2c35
João <joaomarcosxix@gmail.com>
2024-07-07 22:24:01 -0300

    v0 analyzing relative positions

--- game/controls/nose.py
+++ game/controls/nose.py
@@ -1,6 +1,6 @@
 import pygame
 from .base import BaseControl
-from .predictions_helper import get_nose_tip
+from .predictions_helper import get_nose_top, get_nose_bottom, get_nose_tip
 
 class NoseControl(BaseControl):
     def __init__(self, event):
@@ -12,8 +12,16 @@ class NoseControl(BaseControl):
         preds = getattr(self.event, 'predictions', {}).get('predictions', None)
 
         return {
+            'nose_top': get_nose_top(preds),
+            'nose_bottom': get_nose_bottom(preds),
             'nose_tip': get_nose_tip(preds),
         }
+
+    def nose_top(self):
+        return self.predictions()['nose_top']
+
+    def nose_bottom(self):
+        return self.predictions()['nose_bottom']
 
     def nose_tip(self):
         return self.predictions()['nose_tip']
@@ -28,35 +36,39 @@ class NoseControl(BaseControl):
 
     def nose_tilted_left(self):
         nose_tip = self.nose_tip()
-        if nose_tip is None:
+        nose_top = self.nose_top()
+        nose_bottom = self.nose_bottom()
+
+        if nose_tip is None or nose_top is None or nose_bottom is None:
             return False
 
-        # Simple condition: if nose tip is in the left half of the image
-        return nose_tip['x'] < self.image_width() / 2
+        # If the nose tip is more to the left than both the nose top and nose bottom
+        return nose_tip['x'] < nose_top['x'] and nose_tip['x'] < nose_bottom['x']
 
     def nose_tilted_right(self):
         nose_tip = self.nose_tip()
-        if nose_tip is None:
+        nose_top = self.nose_top()
+        nose_bottom = self.nose_bottom()
+
+        if nose_tip is None or nose_top is None or nose_bottom is None:
             return False
 
-        # Simple condition: if nose tip is in the right half of the image
-        return nose_tip['x'] >= self.image_width() / 2
+        # If the nose tip is more to the right than both the nose top and nose bottom
+        return nose_tip['x'] > nose_top['x'] and nose_tip['x'] > nose_bottom['x']
 
     def nose_tilted_up(self):
         nose_tip = self.nose_tip()
-        if nose_tip is None:
-            return False
+        eye_midpoint = {'y': self.image_height() / 2}  # Using the midpoint of the image height for simplicity
 
-        # Simple condition: if nose tip is in the top half of the image
-        return nose_tip['y'] < self.image_height() / 2
+        if nose_tip is not None:
+            return nose_tip['y'] < eye_midpoint['y']
 
     def nose_tilted_down(self):
         nose_tip = self.nose_tip()
-        if nose_tip is None:
-            return False
+        eye_midpoint = {'y': self.image_height() / 2}  # Using the midpoint of the image height for simplicity
 
-        # Simple condition: if nose tip is in the bottom half of the image
-        return nose_tip['y'] >= self.image_height() / 2
+        if nose_tip is not None:
+            return nose_tip['y'] >= eye_midpoint['y']
 
     # Control functions
 
